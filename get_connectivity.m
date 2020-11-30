5c10b4ddeffe6b12c87bebbd5504fb084989c97c
Renata Sadibolova <r.sadibolova@gold.ac.uk>
2020-11-30 17:51:48 +0000

    Revert "testhello"
    This reverts commit 7d9eac0914a580841b10b528e852746f987bf6af.

--- get_connectivity.m
+++ get_connectivity.m
@@ -11,7 +11,6 @@ partnames = struct2cell(findset);
 partnames = transpose(partnames(1,:));
 index = cell2mat(cellfun(@(x) any(strncmp(x,sub(1,:),2) || strncmp(x,sub(2,:),2)), partnames,'UniformOutput',false));
 
-% hello
 
 %gives error if participants are not found 
 if nargin <1 || isnumeric(sub) 
